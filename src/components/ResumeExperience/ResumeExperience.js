745ca63419a2deeeb4964a7c85cc19ba4cda1fd2
Yasmeen-Mostafa <yasmeen.mostafa1236@gmail.com>
2022-09-15 13:59:08 +0300

    second commit

--- src/components/ResumeExperience/ResumeExperience.js
+++ src/components/ResumeExperience/ResumeExperience.js
@@ -3,11 +3,7 @@ import "./ResumeExperience.scss";
 const ResumeExperience = (props) => {
   return (
     <div className="resume__experience">
-      <div className="resume__left">
-        <h3 className="resume__location">{props.experience.location}</h3>
-        <p>{props.experience.date}</p>
-      </div>
-      <div className="resume__shape">
+      <div className="resume__shape test">
         <div className="large__circle">
           <div className="small__circle"></div>
         </div>
@@ -15,9 +11,47 @@ const ResumeExperience = (props) => {
           <hr />
         </div>
       </div>
-      <div className="resume__right">
-        <h3 className="resume__position">{props.experience.position}</h3>
-        <p>{props.experience.details}</p>
+      <div className="display__small__screen">
+        <div className="resume__left">
+          <p className="details">{props.experience.date}</p>
+          <h3 className="resume__position">{props.experience.position}</h3>
+        </div>
+        <div className="resume__shape">
+          <div className="large__circle">
+            <div className="small__circle"></div>
+          </div>
+          <div className="line">
+            <hr />
+          </div>
+        </div>
+        <div className="resume__right">
+          <h3 className="resume__location">{props.experience.location}</h3>
+          {/* <p className="details">{props.experience.details}</p> */}
+          {props.experience.details.map((e, i) => (
+            <p key={i}>{e}</p>
+          ))}
+        </div>
+      </div>
+      <div className="display__large__screen">
+        <div className="resume__left">
+          <h3 className="resume__location">{props.experience.location}</h3>
+          <p className="details">{props.experience.date}</p>
+        </div>
+        <div className="resume__shape">
+          <div className="large__circle">
+            <div className="small__circle"></div>
+          </div>
+          <div className="line">
+            <hr />
+          </div>
+        </div>
+        <div className="resume__right">
+          <h3 className="resume__position">{props.experience.position}</h3>
+          {/* <p className="details">{props.experience.details}</p> */}
+          {props.experience.details.map((e, i) => (
+            <p key={i}>{e}</p>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -6,15 +6,19 @@ let education = [
     location: "Information Technology Institute",
     date: "Mar 2022 - Aug 2022",
     position: "Front-End & Cross-Platform Mobile Developer",
-    details:
+    details: [
       "Technologies: Reactjs, Angular, JavaScript, TypeScript, HTML5, CSS3, SASS, Bootstrap, C++, Git",
+    ],
   },
   {
-    location: "Information Technology Institute",
-    date: "Mar 2022 - Aug 2022",
-    position: "Front-End & Cross-Platform Mobile Developer",
-    details:
-      "Technologies: Reactjs, Angular, JavaScript, TypeScript, HTML5, CSS3, SASS, Bootstrap, C++, Git",
+    location: "Alexandria University",
+    date: "Sep 2016 - Jun 2021",
+    position: "B.Sc. Degree In Computer and Communication Engineering",
+    details: [
+      " Graduated with overall 2.6 GPA.",
+      " Graduation Project: COVID-19 Nurse Robot",
+      " Project Grade: Excellant",
+    ],
   },
 ];
 const Resume = () => {
