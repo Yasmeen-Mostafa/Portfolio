fbaa908a15fe6ea7113a71ac378c5cb50fdb2e15
Yasmeen-Mostafa <yasmeen.mostafa1236@gmail.com>
2022-10-29 15:33:20 +0200

    update responsive screens

--- src/shared/components/Navbar/Navbar.js
+++ src/shared/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavbarIcons from "../NavbarIcons/NavbarIcons";
 import NavbarText from "../NavbarText/NavbarText";
 import "./Navbar.scss";
@@ -7,22 +7,49 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const [expandNav, setExpandNav] = useState(false);
+  const [largeScreen, setLargeScreen] = useState(false);
   const [addAnimation, setAddAnimation] = useState(false);
+  useEffect(() => {
+    const handleWidthChange = (e) => {
+      if (window.innerWidth > 768) setLargeScreen(true);
+      else setLargeScreen(false);
+    };
+    window.addEventListener("resize", handleWidthChange);
+  }, []);
+  useEffect(() => {
+    if (window.innerWidth > 768) {
+      console.log("p");
+      setLargeScreen(true);
+    }
+  }, []);
   const animate = () => {
+    console.log("clicked");
     setAddAnimation(true);
+
     setTimeout(() => {
       setExpandNav((state) => !state);
     }, 500);
   };
   return (
-    <nav>
+    <nav
+      style={{
+        backgroundColor: expandNav ? "#f6f4f2" : "transparent",
+        opacity: expandNav ? "0.95" : "1",
+      }}
+    >
       <button onClick={animate}>
         <FontAwesomeIcon icon={faBars} />
       </button>
-      {expandNav && <NavbarText addAnimation={addAnimation}></NavbarText>}
-      {expandNav === false && (
-        <NavbarIcons addAnimation={addAnimation}></NavbarIcons>
-      )}
+      {largeScreen && <NavbarIcons addAnimation={addAnimation}></NavbarIcons>}
+      <div className="nav__container">
+        {largeScreen ? (
+          <></>
+        ) : (
+          expandNav && <NavbarText addAnimation={addAnimation}></NavbarText>
+        )}
+
+        {/* {expandNav && <NavbarIcons addAnimation={addAnimation}></NavbarIcons>} */}
+      </div>
     </nav>
   );
 };
